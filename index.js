096deaf5140c52455912cad40133b2714fe7bb46
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2024-10-14 15:23:34 +0530

    feat: added new route for update

--- index.js
+++ index.js
@@ -166,6 +166,28 @@ app.delete("/movies/:movieId", async (req,res)=>{
     }
 })
 
+async function updateMovie(movieId, dataToUpdate) {
+    try{
+        const updatedMovie = await Movie.findByIdAndUpdate(movieId, dataToUpdate, {new: true})
+        return updatedMovie
+    }catch (error){
+        console.log("Error in updating Movie rating", error)
+    }
+}
+
+app.post("/movies/:movieId", async (req,res)=>{
+    try{
+        const updatedMovie = await updateMovie(req.params.movieId, req.body)
+        if(updatedMovie){
+            res.status(200).json({message: "Movie updated successfully.", updatedMovie: updatedMovie})
+        }else{
+            res.status(404).json({error: "Movie not found."})
+        }
+    }catch(error){
+        res.status(500).json({error: "failed to update movie."})
+    }
+})
+
 const PORT = 3000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
